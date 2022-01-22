e8d2a14b82854cc521dd80c14104ec88646f6d90
Richard Zhang <rjz23@cam.ac.uk>
2022-01-22 10:42:28 +0000

    Implemented Task 1B (testing and demo program coming later)

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,22 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+from haversine import haversine
+
+def stations_by_distance(stations, p):
+    """ This function takes a list of stations (MonitoringStation
+    type) and a coordinate p (tuple of floats) and calculates the
+    distance (haversine) of each station to the coordinate defined
+    by p.
+    The return is a list of tuples, each of which contains a
+    station (MonitoringStation type) and the corresponding distance (float)
+    """
+
+    distances = []
+    for station in stations:
+        coord = station.coord
+        dist = haversine(coord, p)
+        distances.append((station, dist))
+    
+    distances = sorted_by_key(distances, 1)
+    return distances
