1f96e0d90d0547e239ec5b6945db7ed47963bfd2
Maxwell <maxwell.limingchi@gmail.com>
2022-01-29 22:53:02 +0000

    Added more comments in Task1D.py

--- Task1D.py
+++ Task1D.py
@@ -5,13 +5,20 @@ from floodsystem.stationdata import build_station_list
 def run():
     """Requirements for Task 1D"""
 
-    #Build list of stations
+    #build list of stations
     stations_list = build_station_list()
     rivers = rivers_with_station(stations_list)
+    
+    #get the number of rivers in total
     number_of_rivers = len(rivers)
+
+    #print the number of rivers and the first 10 rivers in alphabetical order
     print("{} stations. First 10 - {}".format(number_of_rivers, sorted(rivers)[:10]))
 
+    #get the rivers and the stations on them
     stations_on_river = stations_by_river(stations_list)
+
+    #print the name of the stations on the selected rivers
     for river in ["River Aire", "River Cam", "River Thames"]:
         station_names = [station.name for station in stations_on_river[river]]
         print("{} has the following stations: {}".format(river, sorted(station_names)))
