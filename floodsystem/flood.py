64eb83a87f9ef78a8826d9e9d5f8087f06ab2976
Richard Zhang <rjz23@cam.ac.uk>
2022-02-18 13:46:35 +0000

    Wrote stations_highest_rel_level (task 2C)

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -17,3 +17,17 @@ def stations_level_over_threshold(stations, tol):
     #sorts the tuples by descending order of water level
     levels = sorted_by_key(levels, 1, reverse=True)
     return levels
+
+def stations_highest_rel_level(stations, N):
+    """ This method takes a list of MonitoringStations objects stations
+    and an integer N, and returns a list of the N stations (MonitoringStations
+    objects) where the relative water level is highest. The returned list
+    will be sorted by descending order of relative level.
+    """
+
+    #create a list of tuples where the relative water level is displayed after
+    #the station, which allows usage of the sorted_by_key method
+    levels = [(station, station.relative_water_level()) for station in stations]
+    levels = sorted_by_key(levels, 1, reverse=True)
+    stations_N = [levels[i][0] for i in range(N)]
+    return stations_N
