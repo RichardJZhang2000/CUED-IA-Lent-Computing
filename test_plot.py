42b691ad3cf177259089d1528047b251db721110
Maxwell <maxwell.limingchi@gmail.com>
2022-02-23 23:14:50 +0000

    added unit test for plot_water_level_with_fit function

--- test_plot.py
+++ test_plot.py
@@ -14,7 +14,7 @@ def test_plot_water_levels():
     update_water_levels(stations)
 
     # Find the 10 stations with highest relative level
-    stations_N = stations_highest_rel_level(stations, 8)
+    stations_N = stations_highest_rel_level(stations, 3)
     
     dt=10
     count = 1
@@ -23,6 +23,29 @@ def test_plot_water_levels():
                                      dt=datetime.timedelta(days=dt))
         if len(levels) > 0:
             plot_water_levels(station, dates, levels)
+            count -= 1
+            if count == 0:
+                break
+
+def test_plot_water_level_with_fit():
+    # Build list of stations
+    stations = build_station_list()
+
+    # Update latest level data for all stations
+    update_water_levels(stations)
+
+    # Find the 10 stations with highest relative level
+    stations_N = stations_highest_rel_level(stations, 3)
+
+    #Plot the water level data of the past 2 days for the 5 stations with the highest levels,
+    #with polynomial fit of degree 4
+    dt=2
+    count = 1
+    for station in stations_N:
+        dates, levels = fetch_measure_levels(station.measure_id,
+                                     dt=datetime.timedelta(days=dt))
+        if len(levels) > 0:
+            plot_water_level_with_fit(station, dates, levels, 4)
             count -= 1
             if count == 0:
                 break
