7dd5225ae23a1de298d8191ebe3b2ec6713ec31c
Richard Zhang <rjz23@cam.ac.uk>
2022-02-25 11:16:43 +0000

    Wrote test function for normalize

--- test_analysis.py
+++ test_analysis.py
@@ -3,7 +3,14 @@
 # SPDX-License-Identifier: MIT
 """Unit test for the analysis module"""
 
-from floodsystem.analysis import polyfit
+from floodsystem.analysis import *
+from floodsystem.stationdata import build_station_list, update_water_levels
 
 def test_polyfit():
     poly, d0 = polyfit([1,2,3],[1,2,3],3)
+
+def test_normalize():
+    stations = build_station_list()
+    update_water_levels(stations)
+    for station in stations:
+        assert station.relative_water_level()==normalize(station.latest_level, station)
