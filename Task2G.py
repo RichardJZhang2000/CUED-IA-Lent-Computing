8f3894a4301e35591520d6f6042eb8b7cfb8194b
Richard Zhang <rjz23@cam.ac.uk>
2022-02-25 11:47:04 +0000

    Wrote demonstration program for task 2G

--- Task2G.py
+++ Task2G.py
@@ -1,15 +1,26 @@
 from floodsystem.stationdata import build_station_list, update_water_levels
-from floodsystem.flood import stations_highest_rel_level
-from floodsystem.plot import plot_water_level_with_fit
-from floodsystem.analysis import polyfit
-from floodsystem.datafetcher import fetch_measure_levels
-import datetime
+from floodsystem.analysis import rate
 import matplotlib as plt
 
 def run():
+    #build the station list and update the water levels
     stations = build_station_list()
     update_water_levels(stations)
 
+    #rate the risks of the stations
+    risk = rate(stations)
+    
+    #convert them to lists based on the level of risk and print those lists
+    severe = [station.name for station in stations if risk[station]=='severe']
+    high = [station.name for station in stations if risk[station]=='high']
+    mod = [station.name for station in stations if risk[station]=='moderate']
+    low = [station.name for station in stations if risk[station]=='low']
+
+    print('Severe:', severe)
+    print('High:', high)
+    print('Moderate:', mod)
+    print('Low:', low)
+    
 
 if __name__ == "__main__":
     print("*** Task 2G: CUED Part IA Flood Warning System ***")
